4b552a9537811fcfdb4471d21f87b4fba1b06159
isaiaherb <146012975+isaiaherb@users.noreply.github.com>
2024-07-01 22:57:11 -0400

    Add files via upload

--- sql/transform_forest_area.sql
+++ sql/transform_forest_area.sql
@@ -1,71 +1,42 @@
-UPDATE forest_area
-SET forest_area_1990_ha = REPLACE(forest_area_1990_ha, ' ', '')  -- Remove spaces
-WHERE forest_area_1990_ha ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
-
-ALTER TABLE forest_area
-    ALTER COLUMN forest_area_1990_ha TYPE INTEGER
-    USING forest_area_1990_ha::INTEGER;
+-- SQL transform statements for the forest_area table --
+--------------------------------------------------------------------------------------------------------------------------------------------
 
 UPDATE forest_area
-SET forest_area_2000_ha = REPLACE(forest_area_2000_ha, ' ', '')  -- Remove spaces
-WHERE forest_area_2000_ha ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
-
-ALTER TABLE forest_area
-    ALTER COLUMN forest_area_2000_ha TYPE INTEGER
-    USING forest_area_2000_ha::INTEGER;
+SET forest_area_proportion_total_land_area_2020 = NULLIF(forest_area_proportion_total_land_area_2020, '...')
+WHERE forest_area_proportion_total_land_area_2020 = '...'; 
 
 UPDATE forest_area
-SET forest_area_2010_ha = REPLACE(forest_area_2010_ha, ' ', '')  -- Remove spaces
-WHERE forest_area_2010_ha ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
+SET forest_area_proportion_total_land_area_2020 = NULLIF(forest_area_proportion_total_land_area_2020, '…')
+WHERE forest_area_proportion_total_land_area_2020 = '…';
 
-ALTER TABLE forest_area
-    ALTER COLUMN forest_area_2010_ha TYPE INTEGER
-    USING forest_area_2010_ha::INTEGER;
-
-UPDATE forest_area
-SET forest_area_2015_ha = REPLACE(forest_area_2015_ha, ' ', '')  -- Remove spaces
-WHERE forest_area_2015_ha ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
-
-ALTER TABLE forest_area
-    ALTER COLUMN forest_area_2015_ha TYPE INTEGER
-    USING forest_area_2015_ha::INTEGER;
-
-UPDATE forest_area
-SET forest_area_2020_ha = REPLACE(forest_area_2020_ha, ' ', '')  -- Remove spaces
-WHERE forest_area_2020_ha ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
-
-ALTER TABLE forest_area
-    ALTER COLUMN forest_area_2020_ha TYPE INTEGER
-    USING forest_area_2020_ha::INTEGER;
-
-UPDATE forest_area
-SET total_land_area_2020_ha = REPLACE(total_land_area_2020_ha, ' ', '')  -- Remove spaces
-WHERE total_land_area_2020_ha ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
-
-ALTER TABLE forest_area
-    ALTER COLUMN total_land_area_2020_ha TYPE INTEGER
-    USING total_land_area_2020_ha::INTEGER;
+--------------------------------------------------------------------------------------------------------------------------------------------
 
 UPDATE forest_area
 SET forest_area_proportion_total_land_area_2020 = REPLACE(forest_area_proportion_total_land_area_2020, ' ', '')  -- Remove spaces
 WHERE forest_area_proportion_total_land_area_2020 ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
 
 ALTER TABLE forest_area
-    ALTER COLUMN forest_area_proportion_total_land_area_2020 TYPE INTEGER
-    USING forest_area_proportion_total_land_area_2020::INTEGER;
+    ALTER COLUMN forest_area_proportion_total_land_area_2020 TYPE NUMERIC
+    USING forest_area_proportion_total_land_area_2020::NUMERIC;
+    
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+UPDATE forest_area
+SET deforestation_2015_2020_ha_per_year = NULLIF(deforestation_2015_2020_ha_per_year, '...')
+WHERE deforestation_2015_2020_ha_per_year = '...'; 
+
+UPDATE forest_area
+SET deforestation_2015_2020_ha_per_year = NULLIF(deforestation_2015_2020_ha_per_year, '…')
+WHERE deforestation_2015_2020_ha_per_year = '…';
+
+--------------------------------------------------------------------------------------------------------------------------------------------
 
 UPDATE forest_area
 SET deforestation_2015_2020_ha_per_year = REPLACE(deforestation_2015_2020_ha_per_year, ' ', '')  -- Remove spaces
 WHERE deforestation_2015_2020_ha_per_year ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
 
 ALTER TABLE forest_area
-    ALTER COLUMN deforestation_2015_2020_ha_per_year TYPE INTEGER
-    USING deforestation_2015_2020_ha_per_year::INTEGER;
-
-UPDATE forest_area
-SET total_forest_area_affected_by_fire_2015_ha = REPLACE(total_forest_area_affected_by_fire_2015_ha, ' ', '')  -- Remove spaces
-WHERE total_forest_area_affected_by_fire_2015_ha ~ '[^-0-9]';  -- Remove non-numeric characters except the minus sign
-
-ALTER TABLE forest_area
-    ALTER COLUMN total_forest_area_affected_by_fire_2015_ha TYPE INTEGER
-    USING total_forest_area_affected_by_fire_2015_ha::INTEGER;
+    ALTER COLUMN deforestation_2015_2020_ha_per_year TYPE NUMERIC
+    USING deforestation_2015_2020_ha_per_year::NUMERIC;
+    
+--------------------------------------------------------------------------------------------------------------------------------------------
